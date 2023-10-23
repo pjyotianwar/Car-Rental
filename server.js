23ff8dcda8683b1cc4dcfd9f09cc961b4dd05d8e
pjyotianwar <70092582+pjyotianwar@users.noreply.github.com>
2023-10-23 11:47:29 +0530

    FC

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV==='production')
 
     app.get('*' , (req , res)=>{
 
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+          res.send('Hello world')//File(path.resolve(__dirname, 'client/build/index.html'));
 
     })
 
